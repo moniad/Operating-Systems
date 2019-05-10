0078b3de5c6d38a4740722474948af8920fe8666
moniad <moni.dziedzic@gmail.com>
2019-05-10 20:48:20 +0200

    Worked a bit on trucker.c, coded the core of all programs

--- Lab7/trucker.c
+++ Lab7/trucker.c
@@ -20,12 +20,16 @@ int shmid, semid;
 key_t semkey, shmkey;
 char *shmdata;
 pid_t child;
-int workersCount;
+int capacity; // X
+int max_pckgsCount_on_the_belt; // K
+int max_pckgsWeight_on_the_belt; // M
 
 void parse_input(int argc, char **argv){
-    if(argc != 2) die_errno("Give me the number of workers!\n");
-    else workersCount = (int) strtol(argv[1], NULL, 10);
-    // printf("%d\n", workersCount);
+    if(argc != 4) die_errno("Give me:\n 1) X - the capacity of the truck\n 2) K - max no of packages on the belt\n 3) M - max total sum of packages' weight\n");
+    capacity = (int) strtol(argv[1], NULL, 10);
+    max_pckgsCount_on_the_belt = (int) strtol(argv[2], NULL, 10);
+    max_pckgsWeight_on_the_belt = (int) strtol(argv[3], NULL, 10);
+    // printf("%d, %d, %d\n", capacity, max_pckgsCount_on_the_belt, max_pckgsWeight_on_the_belt);
 }
 
 void create_and_init_semaphore(){
@@ -58,31 +62,40 @@ void rmv_sem_and_detach_shm(){
 
 int main(int argc, char **argv){
     parse_input(argc, argv);
-    create_and_init_semaphore();
-    create_and_init_shm();
-    child = fork();
-    if(child != 0) atexit(rmv_sem_and_detach_shm);
-    // *shmdata = 0;
-    for(int i=0; i<3; i++){
-        if(child != 0) {
-            if(semop(semid, &take, 1) < 0) die_errno("semop take in parent");
-            if(!fgets(shmdata, SMH_SIZE, stdin)) die_errno("child, gets()");
-            printf("parent - taken: i = %d, shmdata = %s\n", i, shmdata);
-              strncpy(shmdata, "Parent\n", SMH_SIZE);
-          //   *shmdata = 10;
-            if(semop(semid, &give, 1) < 0) die_errno("semop give in parent");
-            sleep(1);
-        }
-        else {
-            if(semop(semid, &take, 1) < 0) die_errno("semop take in child");
-          //   *shmdata = 8;
-            if(!fgets(shmdata, SMH_SIZE, stdin)) die_errno("child, gets()");
-            printf("child - taken: i = %d, shmdata = %s\n", i, shmdata);
-            strncpy(shmdata, "Child\n", SMH_SIZE);
-            if(semop(semid, &give, 1) < 0) die_errno("semop give in child");
-            sleep(1);
-        }
-    }
+    
+    // check if the size of shared memory and the code below are correct 
+
+    // create_and_init_semaphore();
+    // create_and_init_shm();
+
+
+    // TO DO: SIGINT handler? if atexit, it might not be necessary? only write "Handling SIGINT\n" or whatever.
+    // algorytm obsługi taśmy + warunek na masę paczek, pakowanie do ciężarówki
+    // wypisywanie komunikatów u truckera i loaderów
+
+    // child = fork();
+    // if(child != 0) atexit(rmv_sem_and_detach_shm);
+    // // *shmdata = 0;
+    // for(int i=0; i<3; i++){
+    //     if(child != 0) {
+    //         if(semop(semid, &take, 1) < 0) die_errno("semop take in parent");
+    //         if(!fgets(shmdata, SMH_SIZE, stdin)) die_errno("child, gets()");
+    //         printf("parent - taken: i = %d, shmdata = %s\n", i, shmdata);
+    //           strncpy(shmdata, "Parent\n", SMH_SIZE);
+    //       //   *shmdata = 10;
+    //         if(semop(semid, &give, 1) < 0) die_errno("semop give in parent");
+    //         sleep(1);
+    //     }
+    //     else {
+    //         if(semop(semid, &take, 1) < 0) die_errno("semop take in child");
+    //       //   *shmdata = 8;
+    //         if(!fgets(shmdata, SMH_SIZE, stdin)) die_errno("child, gets()");
+    //         printf("child - taken: i = %d, shmdata = %s\n", i, shmdata);
+    //         strncpy(shmdata, "Child\n", SMH_SIZE);
+    //         if(semop(semid, &give, 1) < 0) die_errno("semop give in child");
+    //         sleep(1);
+    //     }
+    // }
     printf("Done\n");
     return 0;
 }
